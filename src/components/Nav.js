ac7c2947e17cb7d2cf072a0ef67393939e76b376
vishnu sharma <vishnu.sharma@ongraph.com>
2022-11-24 16:47:14 +0530

    image url change

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Dummy from "../assets/dummy.jpeg";
 import {useNavigate} from 'react-router-dom'
 
 
@@ -19,7 +18,7 @@ const Nav = () => {
           <>
             {" "}
             <div className="user">
-              <img src={Dummy} alt="user" className="image" />
+              <img src={'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'} alt="user" className="image" />
               <p className="username">{user.name}</p>
             </div>
             <span className="logout" onClick={logout}>
